fd8be4f7e91644520bb758bef484d7811d9c01d6
azly-blt <matthieu.charroin10@gmail.com>
2026-02-13 12:23:11 -0500

    Changement du css et ajout des qlq fonctionalites

--- site/php/accueil.php
+++ site/php/accueil.php
@@ -12,9 +12,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/index.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Accueil</title>
 </head>
 <body>
+    <div class="container">
+        <header>
+            <nav>
+                <a href="accueil.php">Accueil</a>
+                <a href="logout.php">Deconnexion</a>
+                <a href="infoClub.html">Le club</a>
+            </nav>
+        </header>
+    </div>
     <div class="main">
         <h1>Bonjour <span><?php echo htmlspecialchars($_SESSION['user_prenom'] . " " . $_SESSION['user_nom']); ?></span></h1>
         <p>Bienvenue sur ton espace Kayak Polo Verneuil.</p>
